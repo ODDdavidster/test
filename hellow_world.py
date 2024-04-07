132404a01f6451c2abd6e4ec99a72cc693d18ad5
ODDdavidster <legomandavidster@gmail.com>
2024-04-07 11:41:27 -0600

    A Test to see how Github works

---
+++ hellow_world.py
@@ -0,0 +1,12 @@
+print("Hello World")
+if input("type a number from 1 to 6") in [1,2,3,4,5,6]:
+    print("you did a thing")
+print("OwO")
+print("OwO")
+print("OwO")
+print("OwO")
+print("OwO")
+print("OwO")
+print("OwO")
+print("OwO")
+input("end")
